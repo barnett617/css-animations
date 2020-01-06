d3902ec3ccf0daa6270c8be2be733a33e8c22f9c
wangxin <binaryaoe@163.com>
2020-01-06 19:55:46 +0800

    deploy at 2020年 1月 6日 星期一 19时55分46秒 CST

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,25 @@
 # abort on errors
 set -e
 
-# build
+# lint
 echo Linting..
 npm run lint
+
+# build
 echo Building. this may take a minute...
 npm run build
 
+# record public time
 datatime=$(date)
 
+# commit change
 git add -A
 message="deploy at ${datatime}"
 echo message
 git commit -m "${message}"
+
+# synchronize remote
 git push
 
+# publish (push dist directory to remote gh-pages branch)
 npm run publish
