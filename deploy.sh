9802d1c829abd42e7f471ac4802b128cdc5c4014
wangxin <binaryaoe@163.com>
2020-01-06 19:41:14 +0800

    deploy at 2020年 1月 6日 星期一 19时41分14秒 CST

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,11 @@ npm run lint
 echo Building. this may take a minute...
 npm run build
 
+datatime=$(date)
+
+git add -A
+message="deploy at ${datatime}"
+echo message
+git commit -m "${message}"
+
 npm run publish
